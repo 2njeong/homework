835470b11df1fdbf4479e8ecf9d9ed24c28348c3
injeong Hwang <twilight9758@gmail.com>
2024-01-26 17:27:58 +0900

    ex4

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 
+// const initialStudents = [
+//   { name: "Alice", age: 17, grade: "A" },
+//   { name: "Bob", age: 18, grade: "B" },
+//   { name: "Charlie", age: 16, grade: "C" },
+//   { name: "Diana", age: 19, grade: "D" },
+// ];
+
 // TODO: FilterButtons 컴포넌트를 작성하세요. 필터링 옵션을 선택하는 버튼들을 포함해야 합니다.
-function FilterButtons(/* 필요한 props를 여기에 전달하세요 */ {}) {
-  const filterByAge = (minAge) => {};
-  const filterByGrade = (grade) => {};
-  const resetFilter = () => {};
+function FilterButtons(
+  /* 필요한 props를 여기에 전달하세요 */ {
+    initialStudents,
+    setFilteredStudents,
+  }
+) {
+  const filterByAge = (minAge) => {
+    const filltered = initialStudents.filter((stu) => stu.age >= minAge);
+    setFilteredStudents(filltered);
+  };
+  const filterByGrade = (grade) => {
+    const filltered = initialStudents.filter((stu) => stu.grade === grade);
+    setFilteredStudents(filltered);
+  };
+  const resetFilter = () => {
+    setFilteredStudents(initialStudents);
+  };
 
   return (
     <div>
       {/* 여기에 필터링 버튼들을 완성하세요. */}
-      <button>18세 이상</button>
-      <button>A등급</button>
-      <button>필터 초기화</button>
+      <button onClick={() => filterByAge(18)}>18세 이상</button>
+      <button onClick={() => filterByGrade("A")}>A등급</button>
+      <button onClick={resetFilter}>필터 초기화</button>
     </div>
   );
 }
